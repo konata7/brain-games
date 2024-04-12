c3b0f48831bf671c9e8743488580dcbdc3432c91
konata7 <nps777@live.ru>
2024-04-12 12:05:47 +0300

    fix brain-games.js, replace export with export default

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greet = () => {
-    const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+const greet = () => {
+  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default greet;
