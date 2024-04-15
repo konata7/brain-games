99f59a967388e7b1d1983508923842fe23ef1056
konata7 <nps777@live.ru>
2024-04-15 15:27:31 +0300

    refactor: move function isPrime definition out of gameLoop

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,12 +1,12 @@
 import getRandomInt from '../utils.js';
 
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
 const gameLoop = () => {
-  const isPrime = (num) => {
-    for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
-      if (num % i === 0) return false;
-    }
-    return num > 1;
-  };
   const question = getRandomInt(1, 100);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return { question, correctAnswer };
