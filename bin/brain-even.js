da48a9cb438f148c3892043ad8a5e4acad4c8209
konata7 <nps777@live.ru>
2024-04-16 14:59:21 +0300

    fix: incorrect import file path

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import * as brainEven from '../src/games/brain-calc.js';
+import * as brainEven from '../src/games/brain-even.js';
 import runGame from '../src/index.js';
 
 runGame(brainEven.gameTask, brainEven.gameLoop);
