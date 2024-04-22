dbca7ea7e109ae458b96a88f8808ebe608150916
konata7 <nps777@live.ru>
2024-04-22 15:37:53 +0300

    fix: add default handling in calculate() refactor: simplify operator selection

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -13,25 +13,18 @@ const calculate = (operand1, operand2, operator) => {
       res = (operand1 * operand2).toString();
       break;
     default:
+      console.log('E: src/brain-calc.js calculate: got unsupported operator');
   }
   return res;
 };
 
 const gameTask = 'What is the result of the expression?';
 const gameLoop = () => {
-  let operator;
   const operand1 = getRandomInt(0, 100);
   const operand2 = getRandomInt(0, 100);
-  switch (getRandomInt(1, 3)) {
-    case 1:
-      operator = '+';
-      break;
-    case 2:
-      operator = '-';
-      break;
-    default:
-      operator = '*';
-  }
+  const operators = '+-*';
+
+  const operator = operators[getRandomInt(0, operators.length)];
   const question = `${operand1} ${operator} ${operand2}`;
   const correctAnswer = calculate(operand1, operand2, operator);
   return { question, correctAnswer };
