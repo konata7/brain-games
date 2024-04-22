9d5ea2e26b51bdea1e85c742c40d656cc6712921
konata7 <nps777@live.ru>
2024-04-22 15:44:23 +0300

    refactor: split progression generation in 2 functions - getProgression() - getProgressionString()

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,15 +1,14 @@
 import getRandomInt from '../utils.js';
 
-const getProgression = (first, step, length, missingIndex) => {
-  const progression = {
-    sequence: '',
-    missingMember: '',
-  };
-  for (let i = 0; i < length; i += 1) {
-    progression.sequence = i !== missingIndex ? `${progression.sequence}${first + step * i} ` : `${progression.sequence}.. `;
-  }
-  progression.missingMember = (first + step * missingIndex).toString();
+const getProgression = (first, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) progression.push(first + step * i);
   return progression;
+};
+const getProgressionString = (progression, missingIndex) => {
+  const progressionCopy = [...progression];
+  progressionCopy[missingIndex] = '..';
+  return progressionCopy.join(' ');
 };
 
 const gameTask = 'What number is missing in the progression?';
@@ -20,8 +19,8 @@ const gameLoop = () => {
   const missingIndex = getRandomInt(0, length - 1);
 
   const progression = getProgression(first, step, length, missingIndex);
-  const question = progression.sequence;
-  const correctAnswer = progression.missingMember;
+  const question = getProgressionString(progression, missingIndex);
+  const correctAnswer = progression[missingIndex];
   return { question, correctAnswer };
 };
 
